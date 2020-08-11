d286dd5fde279e12cfd13c800cc363fc1a12080c
Scott Morrison <scottmorr75@gmail.com>
2020-08-11 10:14:42 -0600

    fixed package json

--- src/components/Pic/Pic.jsx
+++ src/components/Pic/Pic.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import me from '../../index'; // gives image path
+// import me from '../../index'; // gives image path
 
 function Pic() {
     return (
         <div className="card" style={{ backgroundColor: "green", height: "300px", width: "95%", marginTop: "25px", }}>
 
-            <img className="img" src={process.env.PUBLIC_URL + "/Img/me.jpg"} alt="Card image cap" style = {{height: "100%"}}/>
+            <img className="img" src={process.env.PUBLIC_URL + "/Img/me.jpg"}  style = {{height: "100%"}}/>
 
            
         </div>
